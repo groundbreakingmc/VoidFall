0d34c7fc55f5a399eb744120c28bf52650417754
noslowdwn <noslowdwn@gmail.com>
2024-10-31 15:25:10 +0100

    added new colorizer

---
+++ src/main/java/noslowdwn/voidfall/utils/colorizer/IColorizer.java
@@ -0,0 +1,6 @@
+package noslowdwn.voidfall.utils.colorizer;
+
+public interface IColorizer {
+
+    String colorize(String message);
+}
---
+++ src/main/java/noslowdwn/voidfall/utils/colorizer/VanillaColorizer.java
@@ -0,0 +1,13 @@
+package noslowdwn.voidfall.utils.colorizer;
+
+public final class VanillaColorizer implements IColorizer {
+
+    @Override
+    public String colorize(final String message) {
+        if (message == null || message.isEmpty()) {
+            return message;
+        }
+
+        return ColorCodesTranslator.translateAlternateColorCodes('&', message);
+    }
+}
