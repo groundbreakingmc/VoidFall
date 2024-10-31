ceac34c54b5880818e14b7b08898a9fb1243401f
noslowdwn <noslowdwn@gmail.com>
2024-10-31 15:07:01 +0100

    changed code style

--- src/main/java/noslowdwn/voidfall/utils/Config.java
+++ src/main/java/noslowdwn/voidfall/utils/Config.java
@@ -2,12 +2,12 @@ package noslowdwn.voidfall.utils;
 
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.YamlConfiguration;
+
 import java.io.File;
 
 import static noslowdwn.voidfall.VoidFall.getInstance;
 
-public class Config
-{
+public class Config {
 
     private Config() {
         throw new ExceptionInInitializerError("This class may not be initialized!");
@@ -15,44 +15,35 @@ public class Config
 
     private static File file;
 
-    public static void load()
-    {
+    public static void load() {
         file = new File(getInstance().getDataFolder(), "config.yml");
-        if (!file.exists())
+        if (!file.exists()) {
             getInstance().saveResource("config.yml", false);
+        }
 
         YamlConfiguration config = new YamlConfiguration();
 
-        try
-        {
+        try {
             config.load(file);
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public static void checkVersion()
-    {
+    public static void checkVersion() {
         double version = 1.3;
-        if (getInstance().getConfig().getDouble("config-version") != version || !getInstance().getConfig().getKeys(false).contains("config-version"))
-        {
+        if (getInstance().getConfig().getDouble("config-version") != version || !getInstance().getConfig().getKeys(false).contains("config-version")) {
             int i = 1;
             File backupFile;
-            do
-            {
+            do {
                 backupFile = new File(getInstance().getDataFolder(), "config_backup_" + i + ".yml");
                 i++;
             } while (backupFile.exists());
 
-            if (file.renameTo(backupFile))
-            {
+            if (file.renameTo(backupFile)) {
                 Bukkit.getConsoleSender().sendMessage(ColorsParser.of(null, "&cYour configuration file is old and was renamed to: &7" + backupFile.getName()));
                 load();
-            }
-            else
-            {
+            } else {
                 Bukkit.getConsoleSender().sendMessage(ColorsParser.of(null, "&cYour configuration file is old, but &ncreate new is not possible&c."));
             }
         }
