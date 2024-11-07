8f7dbbbab3a256ee536803cb9e465fe562d434d3
noslowdwn <noslowdwn@gmail.com>
2024-11-07 13:56:58 +0100

    refactored complex method

--- src/main/java/groundbreakingmc/voidfall/actions/TeleportPlayer.java
+++ src/main/java/groundbreakingmc/voidfall/actions/TeleportPlayer.java
@@ -13,89 +13,67 @@ public final class TeleportPlayer extends AbstractAction {
 
     @Override
     public void process(final Player player, final String string) {
-        final String[] params = string.split(";", 6);
+        final String[] params = string.split(";");
 
-        World tpWorld = null;
-        double x = 0, y = 90, z = 0;
-        float yaw = 180f, pitch = 0f;
+        final Location playerLocation = player.getLocation();
+        final World world = this.getWorld(player, params);
+        final double x = this.getCord(playerLocation.getX(), params, 1, 0, "Cord x");
+        final double y = this.getCord(playerLocation.getY(), params, 2, 90, "Cord y");
+        final double z = this.getCord(playerLocation.getZ(), params, 3, 0, "Cord z");
+        final float yaw = this.getLook(playerLocation.getYaw(), params, 4, 180f, "Yaw");
+        final float pitch = this.getLook(playerLocation.getPitch(), params, 5, 0f, "Pitch");
 
-        switch (params.length) {
-            case 6:
-                if (params[5].charAt(0) == '~') {
-                    float arg = Float.parseFloat(params[5].replace("~", ""));
-                    yaw = player.getLocation().getYaw() + arg;
-                } else {
-                    try {
-                        yaw = Float.parseFloat(params[5]);
-                    } catch (final NumberFormatException e) {
-                        super.plugin.getMyLogger().warning("The value: " + params[5] + " specified in \"Yaw\" for the [TELEPORT] action is invalid. Please check your config file.");
-                    }
+        final Location location = new Location(world, x, y, z, yaw, pitch);
+        player.teleport(location);
+    }
+
+    private float getLook(final float current, final String[] params, final int paramNumb, final float defaultValue, final String specifiedIn) {
+        if (params.length > paramNumb) {
+            final String param = params[paramNumb];
+            if (param.charAt(0) == '~') {
+                final float arg = Float.parseFloat(param.replace("~", ""));
+                return current + arg;
+            } else {
+                try {
+                    return Float.parseFloat(param);
+                } catch (final NumberFormatException ignore) {
+                    super.plugin.getMyLogger().warning("The value: " + param + " specified in \"" + specifiedIn + "\" for the [TELEPORT] action is invalid. Please check your config file.");
                 }
-            case 5:
-                if (params[4].charAt(0) == '~') {
-                    float arg = Float.parseFloat(params[4].replace("~", ""));
-                    pitch = player.getLocation().getPitch() + arg;
-                } else {
-                    try {
-                        pitch = Float.parseFloat(params[4]);
-                    } catch (final NumberFormatException e) {
-                        super.plugin.getMyLogger().warning("The value: " + params[4] + " specified in \"Pitch\" for the [TELEPORT] action is invalid. Please check your config file.");
-                    }
-                }
-            case 4:
-                if (params[3].charAt(0) == '~') {
-                    float arg = Float.parseFloat(params[3].replace("~", ""));
-                    z = player.getLocation().getZ() + arg;
-                } else {
-                    try {
-                        z = Double.parseDouble(params[3]);
-                    } catch (final NumberFormatException e) {
-                        super.plugin.getMyLogger().warning("The value: " + params[3] + " specified in \"Cord z\" for the [TELEPORT] action is invalid. Please check your config file.");
-                    }
-                }
-            case 3:
-                if (params[2].charAt(0) == '~') {
-                    float arg = Float.parseFloat(params[2].replace("~", ""));
-                    y = player.getLocation().getY() + arg;
-                } else {
-                    try {
-                        y = Double.parseDouble(params[2]);
-                    } catch (final NumberFormatException e) {
-                        super.plugin.getMyLogger().warning("The value: " + params[2] + " specified in \"Cord y\" for the [TELEPORT] action is invalid. Please check your config file.");
-                    }
-                }
-            case 2:
-                if (params[1].charAt(0) == '~') {
-                    float arg = Float.parseFloat(params[1].replace("~", ""));
-                    x = player.getLocation().getX() + arg;
-                } else {
-                    try {
-                        x = Double.parseDouble(params[1]);
-                    } catch (final NumberFormatException e) {
-                        super.plugin.getMyLogger().warning("The value: " + params[1] + " specified in \"Cord x\" for the [TELEPORT] action is invalid. Please check your config file.");
-                    }
-                }
-            case 1:
-                if (params[0].equals("~")) {
-                    tpWorld = player.getLocation().getWorld();
-                    if (tpWorld == null) {
-                        super.plugin.getMyLogger().warning("The value: " + params[0] + " specified in \"World\" for the [TELEPORT] action is invalid. Please check your config file.");
-                        tpWorld = super.plugin.getServer().getWorlds().get(0);
-                    }
-                } else {
-                    tpWorld = super.plugin.getServer().getWorld(params[0]);
-                }
-            default:
-                final Location location = tpWorld.getSpawnLocation();
-                location.setX(x);
-                location.setY(y);
-                location.setZ(z);
-                location.setPitch(pitch);
-                location.setYaw(yaw);
-                super.plugin.getServer().getScheduler().runTask(super.plugin, () ->
-                    player.teleport(location)
-                );
+            }
         }
+        return defaultValue;
+    }
 
+    private double getCord(final double current, final String[] params, final int paramNumb, final int defaultValue, final String specifiedIn) {
+        if (params.length > paramNumb) {
+            final String param = params[paramNumb];
+            if (param.charAt(0) == '~') {
+                final double arg = Double.parseDouble(param.replace("~", ""));
+                return current + arg;
+            } else {
+                try {
+                    return Double.parseDouble(param);
+                } catch (final NumberFormatException ignore) {
+                    super.plugin.getMyLogger().warning("The value: " + param + " specified in \"" + specifiedIn + "\" for the [TELEPORT] action is invalid. Please check your config file.");
+                }
+            }
+        }
+        return defaultValue;
+    }
+
+    private World getWorld(final Player player, final String[] params) {
+        if (params.length > 0){
+            final String param = params[0];
+            if (param.equals("~")) {
+                return player.getLocation().getWorld();
+            } else {
+                final World tpWorld = super.plugin.getServer().getWorld(param);
+                if (tpWorld != null) {
+                    return tpWorld;
+                }
+                super.plugin.getMyLogger().warning("The value: " + param + " specified in \"World\" for the [TELEPORT] action is invalid. Please check your config file.");
+            }
+        }
+        return super.plugin.getServer().getWorlds().get(0);
     }
 }
