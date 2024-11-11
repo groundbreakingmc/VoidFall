37b04bef807a6965e685b7c353f5f2dd2c207867
noslowdwn <noslowdwn@gmail.com>
2024-11-11 18:21:16 +0100

    effect give action now accept 3 more params

--- src/main/java/groundbreakingmc/voidfall/actions/GiveEffect.java
+++ src/main/java/groundbreakingmc/voidfall/actions/GiveEffect.java
@@ -1,11 +1,10 @@
 package groundbreakingmc.voidfall.actions;
 
 import groundbreakingmc.voidfall.VoidFall;
+import org.bukkit.Color;
 import org.bukkit.entity.Player;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
-
-import static org.bukkit.Bukkit.getScheduler;
 
 public final class GiveEffect extends AbstractAction {
 
@@ -16,36 +15,58 @@ public final class GiveEffect extends AbstractAction {
     @Override
     public void process(final Player player, final String string) {
         final String[] params = string.split(";", 3);
+        final PotionEffectType effect = this.getEffect(params[0]);
+        final int duration = this.getDuration(params);
+        final int amplifier = this.getAmplifier(params);
+        final boolean ambient = params.length > 3 && Boolean.parseBoolean(params[3]);
+        final boolean particles = params.length > 4 && Boolean.parseBoolean(params[4]);
+        final Color color = this.getColor(params);
+        player.addPotionEffect(new PotionEffect(effect, duration, amplifier, ambient, particles, color));
+    }
 
-        PotionEffectType effect = PotionEffectType.BLINDNESS;
-        int duration = 60;
-        int amplifier = 1;
-        switch (params.length) {
-            case 3:
-                try {
-                    duration = Integer.parseInt(params[2]) * 20;
-                } catch (final NumberFormatException e) {
-                    super.plugin.getMyLogger().warning("The value: " + params[2] + " specified in \"Duration\" for the [EFFECT] action is invalid. Please check your config file.");
-                }
-            case 2:
-                try {
-                    amplifier = Integer.parseInt(params[1]);
-                } catch (final NumberFormatException e) {
-                    super.plugin.getMyLogger().warning("The value: " + params[1] + " specified in \"Amplifier\" for the [EFFECT] action is invalid. Please check your config file.");
-                }
-            case 1:
-                effect = PotionEffectType.getByName(params[0]);
-                if (effect == null) {
-                    super.plugin.getMyLogger().warning("The value: " + params[0] + " specified in \"PotionEffect\" for the [EFFECT] action is invalid. Please check your config file.");
-                    effect = PotionEffectType.BLINDNESS;
-                }
-            default:
-                final PotionEffectType fEffect = effect;
-                int fDuration = duration;
-                int fAmplifier = amplifier;
-
-                player.addPotionEffect(new PotionEffect(fEffect, fDuration, fAmplifier));
+    private PotionEffectType getEffect(final String param) {
+        PotionEffectType effect = PotionEffectType.getByName(param);
+        if (effect == null) {
+            super.plugin.getMyLogger().warning("The value: " + param + " specified in \"PotionEffect\" for the [EFFECT] action is invalid. Please check your config file.");
+            effect = PotionEffectType.BLINDNESS;
         }
+        return effect;
+    }
 
+    private int getAmplifier(final String[] params) {
+        if (params.length > 0) {
+            try {
+                return Integer.parseInt(params[1]);
+            } catch (final NumberFormatException ignore) {
+                super.plugin.getMyLogger().warning("The value: " + params[1] + " specified in \"Amplifier\" for the [EFFECT] action is invalid. Please check your config file.");
+            }
+        }
+        return 1;
+    }
+
+    private int getDuration(final String[] params) {
+        if (params.length > 1) {
+            try {
+                return Integer.parseInt(params[2]) * 20;
+            } catch (final NumberFormatException ignore) {
+                super.plugin.getMyLogger().warning("The value: " + params[2] + " specified in \"Duration\" for the [EFFECT] action is invalid. Please check your config file.");
+            }
+        }
+        return 60;
+    }
+
+    private Color getColor(final String[] params) {
+        if (params.length > 5) {
+            try {
+                final String[] colors = params[4].split("\\.");
+                final int red = Integer.parseInt(colors[0]);
+                final int green = Integer.parseInt(colors[1]);
+                final int blue = Integer.parseInt(colors[2]);
+                return Color.fromRGB(red, green, blue);
+            } catch (final NumberFormatException ignore) {
+                super.plugin.getMyLogger().warning("The value: " + params[2] + " specified in \"Duration\" for the [EFFECT] action is invalid. Please check your config file.");
+            }
+        }
+        return null;
     }
 }
